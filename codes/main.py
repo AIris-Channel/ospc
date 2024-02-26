d2e9e6c594071efd7c7f5947c5ac09a8d4248bbd
CjangCjengh <101577701+CjangCjengh@users.noreply.github.com>
2024-02-26 22:41:56 +0800

    label by percent

--- codes/main.py
+++ codes/main.py
@@ -4,15 +4,26 @@ sys.stdout = sys.stderr
 from llava_phi import process_image
 
 
+def find_percent(num_list, percent):
+    n = len(num_list)
+    index = int(n * percent)
+    sorted_a = sorted(num_list)
+    if n % 2 == 0:
+        return (sorted_a[index - 1] + sorted_a[index]) / 2
+    else:
+        return sorted_a[index]
+
+
 if __name__ == '__main__':
+    probs = []
     for line in sys.stdin:
         image_path = line.rstrip()
         try:
-            proba = process_image(image_path)
-            if proba > 0.5:
-                label = 1
-            else:
-                label = 0
-            original_stdout.write(f'{proba:.4f}\t{label}\n')
+            prob = process_image(image_path)
+            probs.append(prob)
         except Exception as e:
             sys.stderr.write(str(e))
+    threshold = find_percent(probs, 0.5)
+    for prob in probs:
+        label = 1 if prob > threshold else 0
+        original_stdout.write(f'{prob:.4f}\t{label}\n')
